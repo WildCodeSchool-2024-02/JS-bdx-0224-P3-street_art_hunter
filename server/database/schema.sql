231e80cd1a60d3e3fd9ea766fffdc823e1be4586
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-06-13 14:53:16 +0200

    update database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -38,12 +38,3 @@ create table creating (
   foreign key(art_id) references art(id),
   foreign key(artist_id) references artist(id)
 );
-
-
-/* A supprimer */
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
