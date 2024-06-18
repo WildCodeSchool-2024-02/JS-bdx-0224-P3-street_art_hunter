27f40464b687b5959e0cad4056aa40ba9c51c306
Dinosaure7 <cherry.pigeons@mailfence.com>
2024-06-18 09:13:20 +0200

    Update Geocoder added SearchBar deleted

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
@@ -15,7 +14,8 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
+    {/* set a provider de leafletGeocoder.jsx? */}
     <RouterProvider router={router} />
   </React.StrictMode>
 );
