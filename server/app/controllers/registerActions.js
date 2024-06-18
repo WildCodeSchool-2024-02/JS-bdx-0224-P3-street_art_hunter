47e86d13314cb7a80e57bf4b2ffa49499f0c780c
Delgorithm <delgorithm@gmail.com>
2024-06-18 17:45:35 +0200

    Added a log to see how my data is being transported

--- server/app/controllers/registerActions.js
+++ server/app/controllers/registerActions.js
@@ -2,6 +2,7 @@ const tables = require("../../database/tables");
 
 const add = async (req, res, next) => {
   const register = req.body;
+  console.info(req.body);
 
   try {
     const insertId = await tables.register.create(register);
