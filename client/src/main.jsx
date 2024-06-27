a2372badf98d8421918b3cd4c58a991fa9204df9
Dinosaure7 <cherry.pigeons@mailfence.com>
2024-06-27 16:16:12 +0200

    Temporarily graphic chart changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode> 
+  <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
