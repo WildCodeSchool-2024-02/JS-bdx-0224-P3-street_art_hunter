0ea4641289e2c1cfd886ed04132457b61a2035e5
Delgorithm <delgorithm@gmail.com>
2024-06-29 14:18:31 +0200

    Fix : Changed method from the FAKER API

--- server/database/fixtures/UserSeeder.js
+++ server/database/fixtures/UserSeeder.js
@@ -16,8 +16,8 @@ class UserSeeder extends AbstractSeeder {
         username: this.faker.internet.userName(),
         city: this.faker.location.city(),
         email: this.faker.internet.email(),
-        password: this.faker.internet.password(),
-        isAdmin: i === 2 ? "1" : "0",
+        hashed_password: this.faker.internet.password(),
+        is_Admin: i === 2 ? "1" : "0",
         registration_date: this.faker.date.past(),
         refName: `user_${i}`, // Create a reference name for the user
       };
