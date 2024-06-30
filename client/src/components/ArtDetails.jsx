abf14a669e0c4b96bff8bdaa50254c0d2f6474b5
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-06-30 21:21:44 +0200

    add modify button + form

--- client/src/components/ArtDetails.jsx
+++ client/src/components/ArtDetails.jsx
@@ -1,7 +1,25 @@
-import "../styles/ArtDetails.css";
+import { useState } from "react";
+import { Form } from "react-router-dom";
 import { PropTypes } from "prop-types";
+import "../styles/ArtDetails.css";
 
 function ArtDetails({ art, onClose }) {
+  const [editMode, setEditMode] = useState(false);
+  const [title, setTitle] = useState(art.title);
+  const [artist, setArtist] = useState(art.artist);
+  const [description, setDescription] = useState(art.description);
+
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Logique pour mettre à jour les informations d'art (title, artist, description)
+    // Exemple : updateArt({ title, artist, description });
+    toggleEditMode(); // Repasser en mode affichage normal après la modification
+  };
+
   return (
     <section className="modal-artDetails">
       <header>
@@ -18,16 +36,55 @@ function ArtDetails({ art, onClose }) {
         <picture className="modal-picture-artDetails">
           <img src={art.image} alt={art.title} />
         </picture>
-        <h2>
-          {art.title ? art.title.toUpperCase() : "TITRE - "}
-          {art.title && art.artist && " - "}
-          {art.artist ? art.artist : "Artiste"}
-        </h2>
-        <p>
-          {art.description
-            ? art.description
-            : "Aucune description n'est disponible pour le moment."}
-        </p>
+        {editMode ? (
+          <Form method="put" onSubmit={handleSubmit}>
+            <label htmlFor="title">Titre</label>{" "}
+            <input
+              type="text"
+              id={title}
+              /* peut-être mettre "title" */
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Titre de l'œuvre"
+            />
+            {/* voir si mettre des value dans les input */}
+            <input
+              type="text"
+              value={artist}
+              onChange={(e) => setArtist(e.target.value)}
+              placeholder="Nom de l'artiste"
+            />
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description de l'œuvre"
+            />
+            <button type="submit">Proposer</button>
+            <button type="button" onClick={toggleEditMode}>
+              Annuler
+            </button>
+          </Form>
+        ) : (
+          <>
+            <h2>
+              {art.title ? art.title.toUpperCase() : "TITRE - "}
+              {art.title && art.artist && " - "}
+              {art.artist ? art.artist : "Artiste"}
+            </h2>
+            <p>
+              {art.description
+                ? art.description
+                : "Aucune description n'est disponible pour le moment."}
+            </p>
+            <button
+          type="button"
+          aria-label="Modifier les informations"
+          onClick={toggleEditMode} // Définissez cette fonction pour gérer le passage en mode édition
+          className="edit-artDetails"
+        >
+          Modifier
+        </button>
+          </>
+        )}
       </article>
     </section>
   );
@@ -45,10 +102,10 @@ ArtDetails.propTypes = {
 
 ArtDetails.defaultProps = {
   art: {
-    image: '',
-    title: '',
-    artist: '',
-    description: '',
+    image: "",
+    title: "",
+    artist: "",
+    description: "",
   },
 };
 
