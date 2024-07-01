bf280e6cf34441d7359dfc741837bda5302b0253
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-07-01 12:15:05 +0200

    first adds to PersonalInfo component

--- client/src/components/PersonalInfo.jsx
+++ client/src/components/PersonalInfo.jsx
@@ -1,12 +1,31 @@
 // import { useLoaderData } from "react-router-dom";
 
+// import { useState } from "react";
+import "../styles/Profile.css"
+
 function PersonalInfo() {
+  // const userInfo = useLoaderData();
 
-    // const userInfo = useLoaderData();
-
-    return(
-        <h2 className="profile-information-title">Mes informations</h2>
-    )
+  return (
+    <section>
+      <h2 className="profile-information-title">Mes informations</h2>
+      <article>
+      <p>Pseudo</p>
+      {/* <p></p> */}
+      <p>Ville</p>
+      {/* <p></p> */}
+      <p>E-mail</p>
+      {/* <p></p> */}
+      </article>
+      <button
+        type="button"
+        aria-label="Modifier les informations"
+        className="profile-information-edit-button"
+      >
+        Modifier
+      </button>
+    </section>
+  );
 }
 
 export default PersonalInfo;
