7cc6c535d7b5091acba0081a56d02a81fa3663ed
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-07-01 11:14:34 +0200

    add Profile page and Info component

---
+++ client/src/components/PersonalInfo.jsx
@@ -0,0 +1,12 @@
+// import { useLoaderData } from "react-router-dom";
+
+function PersonalInfo() {
+
+    // const userInfo = useLoaderData();
+
+    return(
+        <h2 className="profile-information-title">Mes informations</h2>
+    )
+}
+
+export default PersonalInfo;
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import PersonalInfo from "../components/PersonalInfo";
+import "../styles/Profile.css";
+
+function Profile() {
+return (
+    /* {isAdmin && (
+        <p>Page d'administration</p>
+    ) */
+    <PersonalInfo />
+)
+}
+
+export default Profile;
