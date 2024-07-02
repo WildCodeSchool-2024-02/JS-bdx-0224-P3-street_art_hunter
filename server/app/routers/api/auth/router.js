adebad073db62ccb5b2ebf1b9e8140f1d5ce0264
Delgorithm <delgorithm@gmail.com>
2024-07-02 18:01:33 +0200

    Fix : Spelling from 'authAction' to 'authActions'

--- server/app/routers/api/auth/router.js
+++ server/app/routers/api/auth/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const userActions = require("../../../controllers/userAction");
+const userActions = require("../../../controllers/userActions");
 const {
   hashPassword,
   verifyToken,
