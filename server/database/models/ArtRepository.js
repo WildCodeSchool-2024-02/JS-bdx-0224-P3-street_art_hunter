facc0ca5b86dfd05fd40742a717aa604625f91f9
Delgorithm <delgorithm@gmail.com>
2024-07-02 18:02:39 +0200

    Fix : Deleted the browse + Added the correct readAll according to what is needed

--- server/database/models/ArtRepository.js
+++ server/database/models/ArtRepository.js
@@ -5,26 +5,9 @@ class ArtRepository extends AbstractRepository {
     super({ table: "art" });
   }
 
-  async create(art) {
-    const [result] = await this.database.query(
-      `insert into ${this.table} (id, title, information, latitude, longitude, upload_date, status, best_picture) values (?, ?, ?, ?, ?, ?, ?, ?)`,
-      [
-        art.id,
-        art.title,
-        art.information,
-        art.latitude,
-        art.longitude,
-        art.upload_date,
-        art.status,
-        art.best_picture,
-      ]
-    );
-    return result.insertId;
-  }
-
   async readAll() {
     const [rows] = await this.database.query(
-      `SELECT ${this.table}.latitude, ${this.table}.longitude, p.image FROM ${this.table} JOIN picture as p ON p.art_id=${this.table}.id`
+      `SELECT ${this.table}.id, ${this.table}.latitude, ${this.table}.longitude, p.image FROM ${this.table} JOIN picture as p ON p.${this.table}_id=art.id`
     );
     return rows;
   }
