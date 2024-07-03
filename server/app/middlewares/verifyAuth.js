ad1763406ca05205351b29b6bdc67561a9e4816b
Delgorithm <delgorithm@gmail.com>
2024-07-03 22:17:35 +0200

    Fix : Added the middleware to verify the auth

--- server/app/middlewares/verifyAuth.js
+++ server/app/middlewares/verifyAuth.js
@@ -1,5 +1,6 @@
 const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
+const tables = require("../../database/tables");
 
 const hashingOptions = {
   type: argon2.argon2id,
@@ -19,6 +20,43 @@ const hashPassword = async (req, res, next) => {
     delete req.body.password;
 
     next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+const verifyUser = async (req, res, next) => {
+  try {
+    const user = await tables.user.readByEmailWithPassword(req.body.email);
+
+    if (user === null) {
+      res.sendStatus(422);
+      return;
+    }
+
+    const verified = await argon2.verify(
+      user.hashed_password,
+      req.body.password
+    );
+
+    if (verified) {
+      delete user.hashed_password;
+
+      const token = await jwt.sign(
+        { sub: user.id, isAdmin: user.is_admin },
+        process.env.APP_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      res.json({
+        token,
+        user,
+      });
+    } else {
+      res.sendStatus(422);
+    }
   } catch (err) {
     next(err);
   }
@@ -50,4 +88,5 @@ const verifyToken = (req, res, next) => {
 module.exports = {
   hashPassword,
   verifyToken,
+  verifyUser,
 };
