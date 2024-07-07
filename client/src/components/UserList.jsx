90975fea4e202d0a7df1d269bf88b4acf0a15948
Dinosaure7 <cherry.pigeons@mailfence.com>
2024-07-07 15:56:15 +0200

    Update with css

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,24 +1,33 @@
 import { PropTypes } from "prop-types";
 
-function UserList({ user }) {
-
+function UserList({ users }) {
   return (
     <section className="UserList-info-section">
-      <h2 className="UserList-info-title">Liste des utilisateurs</h2>
-      <article className="UserList-info-article">
-      <p>Pseudo</p>
-      <p>{user.username}</p>
-      </article>
+      <h2>Liste des utilisateurs</h2>
+      <table className="table">
+        <thead>
+          <tr className="theader">
+            <th>Pseudo</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody className="cells">
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>37</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
-  )
+  );
 }
 
 UserList.propTypes = {
-    user: PropTypes.shape({
-      username: PropTypes.string.isRequired,
-      city: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-    }).isRequired,
-  };
+  users: PropTypes.arrayOf({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
-export default UserList
+export default UserList;
