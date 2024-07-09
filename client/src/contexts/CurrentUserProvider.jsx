ef92340be38f3e7e1ff133c7909c7fe90a1b0e67
Elea641 <eleavoliotis64@gmail.com>
2024-07-09 16:14:16 +0200

    persistance current user

--- client/src/contexts/CurrentUserProvider.jsx
+++ client/src/contexts/CurrentUserProvider.jsx
@@ -1,10 +1,19 @@
-import { createContext, useState, useMemo } from "react";
+import { createContext, useState, useMemo, useEffect } from "react";
 import PropTypes from "prop-types";
+import decodeTokenAndExtractRole from "../services/decodeToken";
 
 export const CurrentUserContext = createContext();
 
 export function CurrentUserProvider({ children }) {
   const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const userData = decodeTokenAndExtractRole(token);
+      setAuth(userData);
+    }
+  }, []);
 
   const logout = () => {
     setAuth(null);
