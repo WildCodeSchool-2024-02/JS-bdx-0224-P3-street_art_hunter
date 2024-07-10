bb43b8d4df5aaa3e183f69d374b9e47e41ccedf4
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2024-07-10 12:30:37 +0200

    update style mobile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { useContext, useState } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserProvider";
 import ProfileInfo from "../components/ProfileInfo";
 import ProfilePoints from "../components/ProfilePoints";
 import ProfileDelete from "../components/ProfileDelete";
@@ -7,7 +8,7 @@ import "../styles/Profile.css";
 import ProfileContributions from "../components/ProfileContributions";
 
 function Profile() {
-
+  const { auth } = useContext(CurrentUserContext);
   const loaderData = useLoaderData();
   const { userData, pictureData } = loaderData;
   const [isOpen, setIsOpen] = useState(false);
@@ -21,24 +22,31 @@ function Profile() {
   };
 
   return (
-    <>
-      <ProfileInfo user={userData} />
-      <article className="profile-delete-button-wrapper">
-      <button
-        type="submit"
-        aria-label="Supprimer le profil"
-        className="profile-delete-button"
-        onClick={handleOpenModal}
-      >
-        Supprimer mon profil
-      </button>
-      </article>
-      {isOpen && (
-        <ProfileDelete user={userData} onClose={handleCloseModal} />
-      )}
-      <ProfilePoints userData={userData} />
-      <ProfileContributions pictureData={pictureData} />
-    </>
+    <section className="profile-section-wrapper">
+      <section className="profile-section">
+        <article className="link-adminPage-article">
+          {auth?.role === 1 && (
+            <Link to="/admin" className="link-adminPage">
+              Page d'administration
+            </Link>
+          )}
+        </article>
+        <ProfileInfo user={userData} />
+        <article className="profile-delete-button-wrapper">
+          <button
+            type="submit"
+            aria-label="Supprimer le profil"
+            className="profile-delete-button"
+            onClick={handleOpenModal}
+          >
+            Supprimer mon profil
+          </button>
+        </article>
+        {isOpen && <ProfileDelete user={userData} onClose={handleCloseModal} />}
+        <ProfilePoints userData={userData} />
+        <ProfileContributions pictureData={pictureData} />
+      </section>
+    </section>
   );
 }
 
