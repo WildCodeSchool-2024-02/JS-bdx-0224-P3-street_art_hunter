e33feb10911c07e58ef0cbebfe4a0bd1a884b9dc
sorrento974 <sorrento974@gmail.com>
2024-07-10 09:40:42 +0200

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import "../styles/Footer.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer>
+      <ul className="footer">
+        <h2>PixHunt</h2>
+        <Link to="/contact" className="link-inline-item">
+          <li className="inline-item">Contact</li>
+        </Link>
+        <Link to="/credits" className="link-inline-item">
+          <li className="inline-item">Credits</li>
+        </Link>
+        <li className="date">@2024</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
