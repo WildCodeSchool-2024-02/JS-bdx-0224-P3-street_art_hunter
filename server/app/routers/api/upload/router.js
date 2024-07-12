ad0b85922dae63b897f36490d71647bb829e083d
Delgorithm <delgorithm@gmail.com>
2024-07-12 11:37:59 +0200

    Fix : Route for the upload

--- server/app/routers/api/upload/router.js
+++ server/app/routers/api/upload/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const multer = require("multer");
+
+const upload = multer({ dest: "public/tmp/" });
+
 const router = express.Router();
 
 const uploadActions = require("../../../controllers/uploadActions");
 
-router.post("/upload", uploadActions);
+router.post("/", upload.single("file"), uploadActions.add);
 
 module.exports = router;
