db48262d7bb9cc56c218f0ad5e352b4562e5648c
Delgorithm <delgorithm@gmail.com>
2024-07-12 11:32:53 +0200

    Fix : Fix : Added the Form method + Cleaning of the component

--- client/src/pages/Camera.jsx
+++ client/src/pages/Camera.jsx
@@ -1,65 +1,32 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useRef, useCallback } from "react";
+import { Form, useNavigate } from "react-router-dom";
 import Webcam from "react-webcam";
 import "../styles/Camera.css";
 import { MdCameraswitch } from "react-icons/md";
 
-const FACING_MODE_USER = { facingMode: "user" }; // Front Camera
-const FACING_MODE_ENVIRONMENT = { facingMode: { exact: "environment" } }; // Back Camera
-
 function Camera() {
-  const webcamRef = React.useRef(null);
+  const webcamRef = useRef(null);
   const [image, setImage] = useState(null);
-  const [videoConstraints, setVideoConstraints] = useState(FACING_MODE_USER);
   const navigate = useNavigate();
 
-  const getListOfVideoInputs = async () => {
-    const mediaDevices = await navigator.mediaDevices.enumerateDevices();
-    return mediaDevices.filter((device) => device.kind === "videoinput");
-  };
-
-  const switchCamera = async () => {
-    const videoInputList = await getListOfVideoInputs();
-    if (videoInputList.length > 1) {
-      const currentVideoConstraints = { ...videoConstraints };
-
-      if (
-        JSON.stringify(currentVideoConstraints) ===
-        JSON.stringify(FACING_MODE_USER)
-      ) {
-        setVideoConstraints(FACING_MODE_ENVIRONMENT);
-      } else {
-        setVideoConstraints(FACING_MODE_USER);
-      }
-    } else {
-      alert("Device has only one camera.");
-    }
-  };
-
-  const capture = React.useCallback(() => {
+  const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImage(imageSrc);
   }, [webcamRef, setImage]);
 
-  const retake = () => setImage(null);
-
-  const upload = () => {
-    alert("Your photo uploaded successfully!");
-    setImage(null);
-  };
+  const handleRetake = () => setImage(null);
 
   const closeCamera = () => {
     navigate("/");
   };
 
   return (
-    <li className="camera-modal">
-      <img
-        className="logo-pixhunt"
-        src="./src/assets/images/new-logo-pixhunt-Photoroom-removebg-preview.png"
-        alt="logo pixhunt"
-      />
-
+    <Form
+      method="POST"
+      encType="multipart/form-data"
+      action="/camera"
+      className="camera-modal"
+    >
       <button
         type="button"
         aria-labelledby="close-button"
@@ -73,6 +40,7 @@ function Camera() {
         {image ? (
           <li>
             <img src={image} alt="Captured" className="captured-image" />
+            <input type="hidden" name="pictureTaken" value={image} />
           </li>
         ) : (
           <li>
@@ -80,7 +48,6 @@ function Camera() {
               audio={false}
               ref={webcamRef}
               screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
               className="webcam"
             />
           </li>
@@ -93,7 +60,7 @@ function Camera() {
               type="button"
               aria-labelledby="retake-button"
               className="button"
-              onClick={retake}
+              onClick={handleRetake}
             >
               Reprendre
             </button>
@@ -105,7 +72,6 @@ function Camera() {
                 type="button"
                 aria-labelledby="switch-button"
                 className="switch-button"
-                onClick={switchCamera}
               >
                 <MdCameraswitch className="switch-icon" />
               </button>
@@ -123,17 +89,16 @@ function Camera() {
         {image && (
           <li>
             <button
-              type="button"
+              type="submit"
               aria-labelledby="upload-button"
               className="button"
-              onClick={upload}
             >
               Soumettre
             </button>
           </li>
         )}
       </ul>
-    </li>
+    </Form>
   );
 }
 
