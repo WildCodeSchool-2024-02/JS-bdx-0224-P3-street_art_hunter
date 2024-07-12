77ef6fbeb8be2324514a8469f160ced7dc1d1d57
Delgorithm <delgorithm@gmail.com>
2024-07-12 11:36:24 +0200

    Fix : Controller for the upload

--- server/app/controllers/uploadActions.js
+++ server/app/controllers/uploadActions.js
@@ -1,33 +1,51 @@
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 const tables = require("../../database/tables");
 
 const add = async (req, res, next) => {
   try {
+    // console.log("Request received at backend: ", req.file);
+
     if (!req.file) {
+      console.error("No file uploaded");
       return res.status(400).send("No file uploaded");
     }
 
-    const newPath = `public/assets/images/upload/${req.file.originalname}`;
+    const newFileName = `${uuidv4()}.jpg`;
+    const newPath = `public/assets/images/upload/${newFileName}`;
 
-    await fs.rename(req.file.path, newPath);
+    fs.rename(req.file.path, newPath, async (err) => {
+      if (err) {
+        console.error("Error moving file:", err);
+        return next(err);
+      }
 
-    const insertId = await tables.pending.create({
-      image: newPath,
-      user_id: req.body.user_id,
-      art_id: req.body.art_id,
+      const defaultUsedId = 1;
+
+      try {
+        const insertId = await tables.pending.create({
+          image: newPath,
+          user_id: defaultUsedId,
+        });
+
+        // console.log("File uploaded successfully", newPath);
+
+        res.status(201).json({
+          msg: "Upload successful",
+          url: `http://localhost:3000/${newPath}`,
+          insertId,
+        });
+      } catch (dbErr) {
+        console.error("Error saving to database:", dbErr);
+        next(dbErr);
+      }
+      return null;
     });
-
-    res.status(201).json({
-      msg: "Upload successful",
-      url: `http://localhost:3000/${newPath}`,
-      insertId,
-    });
-
-    return null;
   } catch (err) {
+    console.error("Error during file upload", err);
     next(err);
-    return null;
   }
+  return null;
 };
 
 module.exports = { add };
