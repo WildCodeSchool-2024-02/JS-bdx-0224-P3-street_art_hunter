3b4b21ba07949e84814ec4ad330c17a7ab242fac
Delgorithm <delgorithm@gmail.com>
2024-07-14 23:20:01 +0200

    Fix : Added the middleware in the route

--- server/app/routers/api/upload/router.js
+++ server/app/routers/api/upload/router.js
@@ -6,8 +6,10 @@ const upload = multer({ dest: "public/tmp/" });
 
 const router = express.Router();
 
+const { handleFileUpload } = require("../../../middlewares/verifyUpload");
+
 const uploadActions = require("../../../controllers/uploadActions");
 
-router.post("/", upload.single("file"), uploadActions.add);
+router.post("/", upload.single("file"), handleFileUpload, uploadActions.add);
 
 module.exports = router;
