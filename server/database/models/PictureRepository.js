2998ebe09bea205d57f4f38e9137b56e9e52f2f0
Delgorithm <delgorithm@gmail.com>
2024-07-15 12:39:54 +0200

    Picture reporisoty modified

--- server/database/models/PictureRepository.js
+++ server/database/models/PictureRepository.js
@@ -12,6 +12,37 @@ class PictureRepository extends AbstractRepository {
     );
     return rows;
   }
+
+  async create(picture) {
+    const [result] = await this.database.query(
+      `INSERT INTO ${this.table} (image, user_id, art_id, latitude, longitude) VALUES (?, ?, ?, ?, ?)`,
+      [
+        picture.image,
+        picture.user_id,
+        picture.art_id,
+        picture.latitude,
+        picture.longitude,
+      ]
+    );
+
+    return result.insertId;
+  }
+
+  async readByUserId(userId) {
+    const [rows] = await this.database.query(
+      `SELECT * FROM ${this.table} WHERE user_id = ?`,
+      [userId]
+    );
+    return rows;
+  }
+
+  async readByStatus(status) {
+    const [rows] = await this.database.query(
+      `SELECT * FROM ${this.table} WHERE status = ?`,
+      [status]
+    );
+    return rows;
+  }
 }
 
 module.exports = PictureRepository;
