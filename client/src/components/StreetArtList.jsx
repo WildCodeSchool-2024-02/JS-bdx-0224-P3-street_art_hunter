46f189e112849074dd1b916964b5f07bc5c2046e
Dinosaure7 <cherry.pigeons@mailfence.com>
2024-07-15 15:09:35 +0200

    Seeder done

--- client/src/components/StreetArtList.jsx
+++ client/src/components/StreetArtList.jsx
@@ -1,6 +1,7 @@
 import "../styles/AdminStreetArtPage.css";
+import PropTypes from "prop-types";
 
-function StreetArtList() {
+function StreetArtList({images}) {
   return (
     <section className="artlist-section-wrapper">
       <article className="artlist-article">
@@ -8,80 +9,28 @@ function StreetArtList() {
           Oeuvres ajoutées par les utilisateurs
         </h2>
         <ul className="gallery-list">
-          <li className="img1">
-            <img
-              className="image1"
-              src="../src/assets/images/artgallery_2.webp"
-              alt="oeuvre d'art 1"
-            />
-          </li>
-          <li className="img2">
-            <img
-              className="image2"
-              src="../src/assets/images/artgallery11.webp"
-              alt="oeuvre d'art 2"
-            />
-          </li>
-          <li className=" img3">
-            <img
-              className="image3"
-              src="../src/assets/images/artgallery5.webp"
-              alt="oeuvre d'art 3"
-            />
-          </li>
-          <li className=" img4">
-            <img
-              className="image4"
-              src="../src/assets/images/artgallery4.webp"
-              alt="oeuvre d'art 4"
-            />
-          </li>
-          <li className=" img5">
-            <img
-              className="image5"
-              src="../src/assets/images/artgallery7.webp"
-              alt="oeuvre d'art 5"
-            />
-          </li>
-          <li className=" img6">
-            <img
-              className="image6"
-              src="../src/assets/images/artgallery6.webp"
-              alt="oeuvre d'art 6"
-            />
-          </li>
-          <li className=" img7">
-            <img
-              className="image7"
-              src="../src/assets/images/artgallery3.webp"
-              alt="oeuvre d'art 7"
-            />
-          </li>
-          <li className=" img8">
-            <img
-              className="image8"
-              src="../src/assets/images/artgallery9.webp"
-              alt="oeuvre d'art 8"
-            />
-          </li>
-          <li className=" img9">
-            <img
-              className="image9"
-              src="../src/assets/images/artgallery10.webp"
-              alt="oeuvre d'art 9"
-            />
-          </li>
-          <li className=" img10">
-            <img
-              className="image10"
-              src="../src/assets/images/artgallery8.webp"
-              alt="oeuvre d'art 10"
-            />
-          </li>
+          {images.map((image, index) => (
+            <li key={image.id} className={`img${index + 1}`}>
+              <img
+                className={`image${index + 1}`}
+                src={image.src}
+                alt={`oeuvre d'art ${index + 1}`}
+              />
+            </li>
+          ))}
         </ul>
       </article>
     </section>
   );
 }
+
+StreetArtList.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      src: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default StreetArtList;
--- server/database/fixtures/PictureSeeder.js
+++ server/database/fixtures/PictureSeeder.js
@@ -13,21 +13,21 @@ class PictureSeeder extends AbstractSeeder {
 
   run() {
     const images = [
-      {
-        image: "/image_1.jpg",
-      },
-      {
-        image: "/image_2.jpg",
-      },
-      {
-        image: "/image_3.jpg",
-      },
-      {
-        image: "/image_4.jpg",
-      },
-      {
-        image: "/image_5.jpg",
-      },
+      { image: "/image_1.jpg", },
+      { image: "/image_2.jpg", },
+      { image: "/image_3.jpg", },
+      { image: "/image_4.jpg", },
+      { image: "/image_5.jpg", },
+      { image: "/assets/images/artgallery-2.webp" },
+      { image: "/assets/images/artgallery3.webp" },
+      { image: "/assets/images/artgallery4.webp" },
+      { image: "/assets/images/artgallery5.webp" },
+      { image: "/assets/images/artgallery6.webp" },
+      { image: "/assets/images/artgallery7.webp" },
+      { image: "/assets/images/artgallery8.webp" },
+      { image: "/assets/images/artgallery9.webp" },
+      { image: "/assets/images/artgallery10.webp" },
+      { image: "/assets/images/artgallery-11.webp" },
     ];
 
     images.forEach((image, index) => {
@@ -35,6 +35,7 @@ class PictureSeeder extends AbstractSeeder {
         ...image,
         user_id: this.getRef(`user_${index}`).insertId,
         art_id: this.getRef(`art_${index}`).insertId,
+        id: index + 1,
       };
 
       this.insert(imageWithRefName);
