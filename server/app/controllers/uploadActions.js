44c15b00c784ca1f1156410fbfa83ef4a3417be4
Delgorithm <delgorithm@gmail.com>
2024-07-15 11:40:41 +0200

    Upload actions

--- server/app/controllers/uploadActions.js
+++ server/app/controllers/uploadActions.js
@@ -2,9 +2,11 @@ const tables = require("../../database/tables");
 
 const add = async (req, res, next) => {
   try {
-    const insertId = await tables.pending.create({
+    const insertId = await tables.picture.create({
       image: req.newPath,
       user_id: req.body.user_id,
+      latitude: req.latitude,
+      longitude: req.longitude,
     });
 
     res.status(201).json({
