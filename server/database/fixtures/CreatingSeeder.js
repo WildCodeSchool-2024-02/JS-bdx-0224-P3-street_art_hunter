89eed1c3b8fb062491975a048742c993be6b4a27
Delgorithm <delgorithm@gmail.com>
2024-07-22 15:39:51 +0200

    Fix : Creating Seeder

--- server/database/fixtures/CreatingSeeder.js
+++ server/database/fixtures/CreatingSeeder.js
@@ -1,27 +1,34 @@
 const AbstractSeeder = require("./AbstractSeeder");
+const ArtSeeder = require("./ArtSeeder");
+const ArtistSeeder = require("./ArtistSeeder");
 
 class CreatingSeeder extends AbstractSeeder {
   constructor() {
-    super({ table: "creating", truncate: true });
+    super({
+      table: "creating",
+      truncate: true,
+      dependencies: [ArtSeeder, ArtistSeeder],
+    });
   }
 
   run() {
-    const associations = [
-      {},
-      {},
-      {},
-      {},
-      {},
-    ];
+    const associations = [{}, {}, {}, {}, {}];
 
     associations.forEach((association, index) => {
-        const creating = {
-          ... association,
-          art_id: this.getRef(`art_${index}`).insertId,
-          artist_id:this.getRef(`artist_${index}`).insertId,
-        };
+      const artRef = this.getRef(`art_fixed_${index}`);
+      const artistRef = this.getRef(`artist_${index}`);
 
-        this.insert(creating);
+      if (!artRef || !artistRef) {
+        console.error(`Missing reference for art_${index} or artist_${index}`);
+        return;
+      }
+
+      const creating = {
+        art_id: artRef.insertId,
+        artist_id: artistRef.insertId,
+      };
+
+      this.insert(creating);
     });
   }
 }
