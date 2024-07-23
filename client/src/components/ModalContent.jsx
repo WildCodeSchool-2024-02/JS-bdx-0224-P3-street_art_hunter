f480c51a0da82daba6e044055284f80874025909
sorrento974 <sorrento974@gmail.com>
2024-07-23 08:35:51 +0200

    commit before change branch

--- client/src/components/ModalContent.jsx
+++ client/src/components/ModalContent.jsx
@@ -3,18 +3,25 @@ import { PropTypes } from "prop-types";
 function ModalContent({ onClose }) {
   return (
     <section className="modal">
-      <button type="button" className="close-button" onClick={onClose} aria-label="fermer">
+      <button
+        type="button"
+        className="close-button"
+        onClick={onClose}
+        aria-label="fermer"
+      >
         x
       </button>
-      <h2>Bienvenue !</h2>
+      <h2 className="title-modal">Bienvenue !</h2>
       <section className="section2">
-        <p>Avec PixHunt, partez à la chasse aux œuvres d’art urbaines.</p>
+        <p className="modal-text">
+          Avec PixHunt, partez à la chasse aux œuvres d’art urbaines.
+        </p>
         <p>
           Photographiez de nouvelles œuvres d’art à partir de votre téléphone
           pour gagner des points : cliquez sur l’appareil photo et laissez-vous
           guider.
         </p>
-        <p>Explorez, découvrez et amusez-vous!</p>
+        <p className="modal-text">Explorez, découvrez et amusez-vous!</p>
       </section>
     </section>
   );
