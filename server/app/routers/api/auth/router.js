3ba5ca41339d5b7820e577f277c5a4aa8a9c89ae
Delgorithm <delgorithm@gmail.com>
2024-07-23 17:18:23 +0200

    Fix : Deleted

--- server/app/routers/api/auth/router.js
+++ server/app/routers/api/auth/router.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 const userActions = require("../../../controllers/userActions");
 const { verifyUser } = require("../../../middlewares/verifyUser");
-const { verifyToken } = require("../../../middlewares/verifyToken");
 const {
   verifyHashPassword,
 } = require("../../../middlewares/verifyHashPassword");
@@ -13,6 +12,5 @@ router.post("/users", verifyHashPassword, userActions.add);
 
 router.post("/login", verifyUser);
 
-router.use(verifyToken);
 
 module.exports = router;
